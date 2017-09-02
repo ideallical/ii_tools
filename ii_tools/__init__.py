c9947d0f96638f2138f66051962a5547dbb4af06
Michael van de Waeter <mvdwaeter@gmail.com>
2017-09-02 15:28:33 +0200

    Added flake8 support with one flake8 mistake

--- ii_tools/__init__.py
+++ ii_tools/__init__.py
@@ -7,16 +7,16 @@ from six import string_types
 __all__ = ('remove_all_capitals_except_dsq')
 
 
-def remove_all_capitals_except_dsq(s):
+def remove_all_capitals_except_dsq(string_value):
     """
     Remove all capitals except the letters D, S & Q.
 
-    :param str s: The string to filter
+    :param str string_value: The string to filter
     :return: the filtered string
     :raises: ValueError: if the s is not a string_type
     """
-    if isinstance(s, string_types):
+    if isinstance(string_value, string_types):
         return ''.join(
-            [c for c in s if (c not in string.ascii_uppercase) or c in 'DSQ'])
+            [c for c in string_value if (c not in string.ascii_uppercase) or c in 'DSQ'])
     else:
         raise ValueError
