4e0e2b6d67cbc328805ab75521a2ef6e0a9f1859
Michael van de Waeter <mvdwaeter@gmail.com>
2017-09-02 15:51:55 +0200

    Made flake8 more strict and added docstrings

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
 
-setup(name='ii_tools',
-      version='0.1',
-      description='ideallical tools',
-      url='https://github.com/ideallical/ii_tools',
-      author='ideallical',
-      author_email='info@ideallical.com',
-      license='BSD',
-      install_requires=[
-            'six>=1.10.0',
-      ],
-      packages=['ii_tools'],
-      zip_safe=False)
+setup(
+    name='ii_tools',
+    version='0.1',
+    description='ideallical tools',
+    url='https://github.com/ideallical/ii_tools',
+    author='ideallical',
+    author_email='info@ideallical.com',
+    license='BSD',
+    install_requires=[
+        'six>=1.10.0',
+    ],
+    packages=['ii_tools'],
+    zip_safe=False
+)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,10 +8,12 @@ from ii_tools import remove_all_capitals_except_dsq
     ('Lorem ipsum DunsimQ', 'orem ipsum DunsimQ'),
 ])
 def test_remove_all_capitals_except_dsq(test_input, expected):
+    """Test that function works as expected."""
     assert remove_all_capitals_except_dsq(test_input) == expected
 
 
 @pytest.mark.parametrize('test_input', [None, {}, [], object(), 1])
 def test_remove_all_capitals_except_dsq_value_errors(test_input):
+    """Test that function raises ValueError for non-string types."""
     with pytest.raises(ValueError):
         remove_all_capitals_except_dsq(test_input)
